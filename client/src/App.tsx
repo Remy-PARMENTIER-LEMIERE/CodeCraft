41322b467ac58a2fd9574530d751257ed9bb9561
darasom69 <daha69@hotmail.fr>
2025-05-22 11:50:37 +0200

    Add_editor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router";
 
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,9 +1,12 @@
 import "./Home.css"
+import Editor from "../../components/Editor/Editor"
 
 export default function Home() {
     return (
+        <section className="home">
         <article className="home">
             <h1>Je suis une page d'accueil</h1>
         </article>
+        <Editor /></section>
     )
 }
