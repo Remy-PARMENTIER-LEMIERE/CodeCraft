229246925573a45cba14a63d7c922436d6fb4ccc
Remy-PARMENTIER-LEMIERE <remyparmentierlemiere@hotmail.fr>
2025-05-23 09:56:50 +0200

    work in progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Outlet } from "react-router";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
