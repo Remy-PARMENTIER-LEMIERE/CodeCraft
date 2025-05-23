96dc9e81979b22bc8a31acaf86c6711e56662916
B-Maxp <max.bpr@outlook.fr>
2025-05-23 09:31:51 +0200

    resolved layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,14 @@
 import "./App.css";
 import { Outlet } from "react-router";
 import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
 import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <>
-      <Layout/>
-      {/* <Navbar /> */}
-      {/* <Header /> */}
-      <Outlet />
+      <Layout>
+        <Outlet />
+      </Layout>
       <Footer />
     </>
   );
